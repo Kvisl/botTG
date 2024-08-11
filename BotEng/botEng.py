d39bea288d6f9d0874602c63a93ae64cdd2f1acc
SVG <tanngniostr@yahoo.com>
2024-08-11 23:58:22 +0300

    change

--- BotEng/botEng.py
+++ BotEng/botEng.py
@@ -3,32 +3,28 @@ import random
 from dotenv import load_dotenv
 from telebot import types, TeleBot, custom_filters
 from telebot.handler_backends import State, StatesGroup
-from sqlalchemy.sql.expression import func
-from models import create_table, Word, Translate, Other, User
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sqlalchemy
+from models import Base, Word, CommonWord, User
 
 load_dotenv()
 
 LOGIN = os.getenv('LOGIN')
 PASSWORD = os.getenv('PASSWORD')
 NAMEDB = os.getenv('NAMEDB')
-
 TOKEN = os.getenv('TOKEN')
+
 bot = TeleBot(TOKEN)
 
-
 DSN = f'postgresql://{LOGIN}:{PASSWORD}@localhost/{NAMEDB}'
-engine = sqlalchemy.create_engine(DSN)
-create_table(engine)
-
-
+engine = create_engine(DSN)
+Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
 class Command:
-    ADD_WORD = 'Добавить слов\u2795'
+    ADD_WORD = 'Добавить слово\u2795'
     DELETE_WORD = 'Удалить слово\u2B05'
     NEXT = 'Дальше\u23ED'
 
@@ -36,30 +32,42 @@ class Command:
 class MyStates(StatesGroup):
     target_word = State()
     translate_word = State()
-    other_words = State()
-    example_word = State()
     add_word = State()
     delete_word = State()
+
+
+def get_random_word(user_id):
+
+    user_words = session.query(Word).filter_by(user_id=user_id).all()
+    common_words = session.query(CommonWord).all()
+
+    all_words = user_words + common_words
+    if all_words:
+        return random.choice(all_words)
+    return None
 
 
 def create_cards(message):
     user_id = message.from_user.id
     markup = types.ReplyKeyboardMarkup(row_width=2)
 
-    word = (session.query(Word).filter((Word.user_id == user_id) | (Word.user_id == 1), Word.mastered == False)
-            .order_by(func.random()).first())
+    word = get_random_word(user_id)
 
     if word:
-        target_word = word.target_word
-        translate_object = session.query(Translate).filter_by(id_word=word.id).first()
-        translate_word = translate_object.translate_word
-        example = translate_object.example
-        other_words = [other.other_word for other in session.query(Other).filter_by(id_translate=word.id).all()]
+        if isinstance(word, Word):
+            target_word = word.target_word
+            translate_word = word.translate_word
+        elif isinstance(word, CommonWord):
+            target_word = word.target_word
+            translate_word = word.translate_word
+        else:
+            bot.send_message(message.chat.id, "Произошла ошибка! Попробуйте позже.")
+            return
 
-        target_word_btn = types.KeyboardButton(translate_word)
-        other_word_btn = [types.KeyboardButton(word) for word in other_words]
-
-        buttons = [target_word_btn] + other_word_btn
+        all_words = session.query(Word.translate_word).filter(Word.target_word != target_word).all()
+        all_words.extend(session.query(CommonWord.translate_word).filter(CommonWord.target_word != target_word).all())
+        other_words = random.sample(all_words, 3)
+        buttons = [types.KeyboardButton(translate_word)] + [types.KeyboardButton(w[0]) for w in other_words]
         random.shuffle(buttons)
 
         next_btn = types.KeyboardButton(Command.NEXT)
@@ -74,8 +82,8 @@ def create_cards(message):
         with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data['target_word'] = target_word
             data['translate_word'] = translate_word
-            data['other_words'] = other_words
-            data['example'] = example
+    else:
+        bot.send_message(message.chat.id, "Вы изучили все слова! Добавьте новые слова для изучения.")
 
 
 @bot.message_handler(commands=['cards', 'start'])
@@ -86,9 +94,6 @@ def start_bot(message):
         new_user = User(id=user_id)
         session.add(new_user)
         session.commit()
-
-
-
     bot.send_message(message.chat.id,
                      "Здравствуйте 👋\n\n Давайте улучшим свои навыки в английском языке.\n "
                      "Ты можешь выбрать свой собственный темп обучения, чтобы максимально эффективно использовать время.\n\n"
@@ -136,59 +141,18 @@ def add_word_translate(message):
     translate_word = message.text
     with bot.retrieve_data(message.from_user.id, cid) as data:
         data['translate_word'] = translate_word
-    bot.send_message(cid, "Введите три неправильных варианта перевода, разделяя их запятыми \U0001f1ec\U0001f1e7:")
-    bot.set_state(message.from_user.id, MyStates.other_words, cid)
-
-
-@bot.message_handler(state=MyStates.other_words)
-def add_word_other(message):
-    cid = message.chat.id
-    other_words = message.text.split(",")
-    with bot.retrieve_data(message.from_user.id, cid) as data:
-        data['other_words'] = other_words
         target_word = data['target_word']
-        translate_word = data['translate_word']
         user_id = message.from_user.id
 
-        new_word = Word(target_word=target_word, user_id=user_id)
-        session.add(new_word)
-        session.flush()
+    new_word = Word(target_word=target_word, translate_word=translate_word, user_id=user_id)
+    session.add(new_word)
+    session.commit()
 
-        new_translate = Translate(translate_word=translate_word, word=new_word)
-        session.add(new_translate)
+    bot.send_message(cid, "Слово успешно добавлено!")
 
-        for other_word in other_words:
-            new_other = Other(other_word=other_word.strip(), translate=new_translate)
-            session.add(new_other)
+    total_words = session.query(Word).filter_by(user_id=user_id).count()
 
-        session.commit()
-
-        bot.send_message(cid, f"Теперь введите пример использования слова '{translate_word}' "
-                              f"на английском \U0001f1ec\U0001f1e7:")
-        bot.set_state(message.from_user.id, MyStates.example_word,cid)
-
-
-@bot.message_handler(state=MyStates.example_word)
-def add_example(message):
-    cid = message.chat.id
-    example = message.text
-    user_id = message.from_user.id
-
-    with bot.retrieve_data(user_id, cid) as data:
-        target_word = data['target_word']
-
-    word = session.query(Word).filter_by(target_word=target_word).first()
-
-    if word:
-        translate = session.query(Translate).filter_by(id_word=word.id).first()
-        translate.example = example
-        session.commit()
-
-        bot.send_message(cid, "Слово с примером успешно добавлено!")
-
-        total_words = session.query(Word).filter_by(user_id=user_id).count()
-
-        bot.send_message(cid, f"Вы изучаете {total_words} слова.")
+    bot.send_message(cid, f"Вы изучаете {total_words} слова.")
 
     create_cards(message)
 
@@ -196,7 +160,7 @@ def add_example(message):
 @bot.message_handler(func=lambda message: message.text == Command.DELETE_WORD)
 def delete_word_start(message):
     cid = message.chat.id
-    bot.send_message(cid, "Введите слово, которое хотите удалить \U0001F1F7\U0001F1FA:")
+    bot.send_message(cid, "Введите слово на русском, которое хотите удалить \U0001F1F7\U0001F1FA:")
     bot.set_state(message.from_user.id, MyStates.delete_word, cid)
 
 
@@ -225,26 +189,10 @@ def message_reply(message):
 
     with bot.retrieve_data(message.from_user.id, cid) as data:
         translate_word = data['translate_word']
+        target_word = data['target_word']
 
     if text == translate_word:
-        user_id = message.from_user.id
-        word = session.query(Word).filter_by(target_word=data['target_word'], user_id=user_id).first()
-
-        if word:
-            word.attempts += 1
-            if word.attempts >= 5:
-                word.mastered = True
-                answer = (f"Отлично!\U0001f600\n{data['target_word']} -> \U0001f1ec\U0001f1e7 {data['translate_word']} "
-                          f"-> {data['example']}\n\U0001f389 Слово '{data['translate_word']}' изучено! \U0001f389")
-            else:
-                answer = (f"Отлично!\U0001f600\n{data['target_word']} -> \U0001f1ec\U0001f1e7 {data['translate_word']} "
-                          f"-> {data['example']}\nОсталось {5 - word.attempts} "
-                          f"правильных ответа до изучения слова '{data['translate_word']}'.")
-            session.commit()
-        else:
-            answer = (f"Отлично!\U0001f600\n{data['target_word']} -> \U0001f1ec\U0001f1e7 "
-                      f"{data['translate_word']} -> {data['example']}")
-
+        answer = (f"Отлично!\U0001f600\n{target_word} -> \U0001f1ec\U0001f1e7 {translate_word}")
     else:
         answer = "\u274CДопущена ошибка! Попробуйте ещё раз"
 
@@ -261,6 +209,3 @@ bot.add_custom_filter(custom_filters.StateFilter(bot))
 if __name__ == '__main__':
     print('Бот запущен!')
     bot.infinity_polling(skip_pending=True)
-
-
-
--- BotEng/models.py
+++ BotEng/models.py
@@ -1,6 +1,7 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import declarative_base, relationship
 
+
 Base = declarative_base()
 
 
@@ -8,36 +9,26 @@ class Word(Base):
     __tablename__ = 'word'
 
     id = sa.Column(sa.Integer, primary_key=True)
-    target_word = sa.Column(sa.String(length=50), nullable=False, unique=True)
-    translate = relationship('Translate', backref='word', cascade='all')
-    user_id = sa.Column(sa.BigInteger, sa.ForeignKey('user.id'), nullable=False)
-    attempts = sa.Column(sa.Integer, default=0)
-    mastered = sa.Column(sa.Boolean, default=False)
-
-
-class Translate(Base):
-    __tablename__ = 'translate'
-
-    id = sa.Column(sa.Integer, primary_key=True)
+    target_word = sa.Column(sa.String(length=50), nullable=False)
     translate_word = sa.Column(sa.String(length=50), nullable=False)
-    id_word = sa.Column(sa.Integer, sa.ForeignKey('word.id'), nullable=False)
-    other_words = relationship('Other', backref='translate', cascade='all')
-    example = sa.Column(sa.String(length=255))
+    user_id = sa.Column(sa.BigInteger, sa.ForeignKey('user.id'), nullable=True)
 
 
-class Other(Base):
-    __tablename__ = 'other'
+class CommonWord(Base):
+    __tablename__ = 'common_word'
 
     id = sa.Column(sa.Integer, primary_key=True)
-    other_word = sa.Column(sa.String(length=50), nullable=False)
-    id_translate = sa.Column(sa.Integer, sa.ForeignKey('translate.id'), nullable=False)
+    target_word = sa.Column(sa.String(length=50), nullable=False)
+    translate_word = sa.Column(sa.String(length=50), nullable=False)
 
 
 class User(Base):
     __tablename__ = 'user'
 
     id = sa.Column(sa.BigInteger, primary_key=True)
+    words = relationship('Word', backref='user')
 
 
 def create_table(engine):
+
     Base.metadata.create_all(engine)
